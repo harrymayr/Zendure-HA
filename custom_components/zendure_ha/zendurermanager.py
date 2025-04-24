fc1c31e31df426eef1b4bb7fd3f0aa3d3f4dd23a
FireSon <6523983+FireSon@users.noreply.github.com>
2025-04-24 23:03:11 +0000

    fix solarInputPower

--- custom_components/zendure_ha/zendurermanager.py
+++ custom_components/zendure_ha/zendurermanager.py
@@ -352,7 +352,7 @@ class ZendureManager(DataUpdateCoordinator[int]):
             # get the current power, exit if a device is waiting
             powerActual = 0
             for d in ZendureDevice.devices:
-                d.powerAct = d.asInt("packInputPower") - d.asInt("outputPackPower") - d.asInt("solarInputPower")
+                d.powerAct = d.asInt("packInputPower") - (d.asInt("outputPackPower") - d.asInt("solarInputPower"))
                 powerActual += d.powerAct
 
             _LOGGER.info(f"Update p1: {p1} power: {powerActual} operation: {self.operation}")
