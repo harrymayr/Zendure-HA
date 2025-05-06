1fce6b5455a6a32aa3102277225df283313ceeb3
harrymayr <104222469+harrymayr@users.noreply.github.com>
2025-05-06 18:58:32 +0200

    Update hub1200.py
    Output for Hub 1200 works with this changes

--- custom_components/zendure_ha/devices/hub1200.py
+++ custom_components/zendure_ha/devices/hub1200.py
@@ -91,12 +91,7 @@ class Hub1200(ZendureDevice):
             "arguments": [
                 {
                     "autoModelProgram": 2 if inprogram else 0,
-                    "autoModelValue": {
-                        "chargingType": 0 if power >= 0 else 1,
-                        "chargingPower": 0 if power >= 0 else -power,
-                        "freq": 2 if delta < 100 else 1 if delta < 200 else 0,
-                        "outPower": max(0, power),
-                    },
+                    "autoModelValue": power,
                     "msgType": 1,
                     "autoModel": 8 if inprogram else 0,
                 }
