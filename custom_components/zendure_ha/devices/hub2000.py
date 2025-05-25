55f25c40384e003f809da126e88e84fc338208ae
harrymayr <104222469+harrymayr@users.noreply.github.com>
2025-05-25 20:24:10 +0200

    Update hub2000.py
    removed binary sensor autoRecover

--- custom_components/zendure_ha/devices/hub2000.py
+++ custom_components/zendure_ha/devices/hub2000.py
@@ -35,7 +35,6 @@ class Hub2000(ZendureDevice):
             self.binary("heatState"),
             self.binary("reverseState"),
             self.binary("pass"),
-            self.binary("autoRecover"),
         ]
         ZendureBinarySensor.add(binaries)
 
