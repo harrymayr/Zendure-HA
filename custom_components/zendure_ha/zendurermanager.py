04b5b2d2b737e7650c04d9496048963ed9d046b6
Jan Brandsma <6523983+FireSon@users.noreply.github.com>
2025-06-04 06:28:21 +0000

    fix bleErr

--- custom_components/zendure_ha/zendurermanager.py
+++ custom_components/zendure_ha/zendurermanager.py
@@ -238,7 +238,7 @@ class ZendureManager(DataUpdateCoordinator[int], ZendureBase):
 
             for device in ZendureDevice.devices:
                 # Reset MQTT server each day and when it is not responding
-                if midnight or (checkreset and (device.mqttLocal + device.mqttZendure == 0 or self.bleErr)):
+                if midnight or (checkreset and (device.mqttLocal + device.mqttZendure == 0 or device.bleErr)):
                     await device.bleMqtt()
 
                 # check for bluetooth device
