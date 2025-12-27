dcbfba1fcaa60b60545e09bfb90178906505f88a
harrymayr <104222469+harrymayr@users.noreply.github.com>
2025-12-27 07:59:46 +0100

    Refactor p1meter event listener management
    Deregister previous listeners before setting new ones for p1meter events.

--- custom_components/zendure_ha/manager.py
+++ custom_components/zendure_ha/manager.py
@@ -351,19 +351,23 @@ class ZendureManager(DataUpdateCoordinator[None], EntityDevice):
             async_track_state_change_event(self.hass, p1meter, sensor_changed)
 
 
-        if self.p1meterEvent:
-            self.p1meterEvent()
+        # alte Listener deregistrieren 
+        if self.p1meterEvent: 
+            for unsub in self.p1meterEvent: 
+                unsub()
         if p1meter:
-            self.p1meterEvent = async_track_state_change_event(
-                self.hass,
-                [p1meter],
-                self._p1_event_wrapper
-            )            
-            self.p1meterEvent = async_track_state_change_event(
-                self.hass,
-                [self.p1meter.entity_id],
-                self._p1_event_wrapper
-            )            
+            self.p1meterEvent = [
+                async_track_state_change_event(
+                    self.hass,
+                    [p1meter],
+                    self._p1_event_wrapper
+                ),            
+                async_track_state_change_event(
+                    self.hass,
+                    [self.p1meter.entity_id],
+                    self._p1_event_wrapper
+                )
+            ]                
             if (entity := self.hass.states.get(p1meter)) is not None and entity.attributes.get("unit_of_measurement", "W") in ("kW", "kilowatt", "kilowatts"):
                 self.p1_factor = 1000
         else:
