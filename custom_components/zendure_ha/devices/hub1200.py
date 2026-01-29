7548d0317978477d7d9091126363e33fc4ed87ad
harrymayr <104222469+harrymayr@users.noreply.github.com>
2026-01-29 20:04:42 +0100

    Change Hub1200 limits from -800 to 0

--- custom_components/zendure_ha/devices/hub1200.py
+++ custom_components/zendure_ha/devices/hub1200.py
@@ -14,7 +14,7 @@ class Hub1200(ZendureLegacy):
     def __init__(self, hass: HomeAssistant, deviceId: str, prodName: str, definition: Any) -> None:
         """Initialise Hub1200."""
         super().__init__(hass, deviceId, definition["deviceName"], prodName, definition)
-        self.setLimits(-800, 800)
+        self.setLimits(0, 800)
         self.maxSolar = -800
 
     def batteryUpdate(self, batteries: list[ZendureBattery]) -> None:
