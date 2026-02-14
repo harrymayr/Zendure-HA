d0b03eb8f28f6a73d01fff8ae7acbf4fa7aa6dce
harrymayr <104222469+harrymayr@users.noreply.github.com>
2026-02-14 16:18:04 +0100

    Update aio2400.py charge-limit

--- custom_components/zendure_ha/devices/aio2400.py
+++ custom_components/zendure_ha/devices/aio2400.py
@@ -1,4 +1,4 @@
-"""Module for the Hyper2000 device integration in Home Assistant."""
+"""Module for the AIO 2400 device integration in Home Assistant."""
 
 import logging
 from typing import Any
@@ -15,30 +15,13 @@ class AIO2400(ZendureLegacy):
     def __init__(self, hass: HomeAssistant, deviceId: str, prodName: str, definition: Any) -> None:
         """Initialise AIO2400."""
         super().__init__(hass, deviceId, definition["deviceName"], prodName, definition)
-        self.setLimits(-1200, 1200)
+        """AIO 2400 cannot charge using AC"""
+        self.setLimits(0, 1200)
         self.maxSolar = -1200
 
     async def charge(self, power: int) -> int:
-        _LOGGER.info(f"Power charge {self.name} => {power}")
-        self.mqttInvoke(
-            {
-                "arguments": [
-                    {
-                        "autoModelProgram": 2,
-                        "autoModelValue": {
-                            "chargingType": 1,
-                            "chargingPower": -power,
-                            "freq": 0,
-                            "outPower": 0,
-                        },
-                        "msgType": 1,
-                        "autoModel": 8,
-                    }
-                ],
-                "function": "deviceAutomation",
-            }
-        )
-        return power
+        _LOGGER.info(f"No AC charge for {self.name} available")
+        return 0
 
     async def discharge(self, power: int) -> int:
         _LOGGER.info(f"Power discharge {self.name} => {power}")
