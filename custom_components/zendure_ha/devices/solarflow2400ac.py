45a5371fa5b552ad92f83b28767637907365a6ed
harrymayr <104222469+harrymayr@users.noreply.github.com>
2026-02-14 16:21:12 +0100

    Add SF 2400 AC+ correct charge-limits for SF 2400 Pro

--- custom_components/zendure_ha/devices/solarflow2400ac.py
+++ custom_components/zendure_ha/devices/solarflow2400ac.py
@@ -10,7 +10,20 @@ from custom_components.zendure_ha.sensor import ZendureRestoreSensor, ZendureSen
 
 _LOGGER = logging.getLogger(__name__)
 
+class SolarFlow2400AC_Plus(ZendureZenSdk):
+    def __init__(self, hass: HomeAssistant, deviceId: str, prodName: str, definition: Any) -> None:
+        """Initialise SolarFlow2400AC."""
+        super().__init__(hass, deviceId, definition["deviceName"], prodName, definition)
+        self.setLimits(-3200, 2400)
+        self.maxSolar = -2400
+        self.offGrid = ZendureSensor(self, "gridOffPower", None, "W", "power", "measurement")
+        self.aggrOffGrid = ZendureRestoreSensor(self, "aggrGridOffPowerTotal", None, "kWh", "energy", "total", 2)
 
+    @property
+    def pwr_offgrid(self) -> int:
+        """Get the offgrid power."""
+        return self.offGrid.asInt
+        
 class SolarFlow2400AC(ZendureZenSdk):
     def __init__(self, hass: HomeAssistant, deviceId: str, prodName: str, definition: Any) -> None:
         """Initialise SolarFlow2400AC."""
@@ -30,8 +43,8 @@ class SolarFlow2400Pro(ZendureZenSdk):
     def __init__(self, hass: HomeAssistant, deviceId: str, prodName: str, definition: Any) -> None:
         """Initialise SolarFlow2400AC."""
         super().__init__(hass, deviceId, definition["deviceName"], prodName, definition)
-        self.setLimits(-2400, 2400)
-        self.maxSolar = -2400
+        self.setLimits(-3200, 2400)
+        self.maxSolar = -3000
         self.offGrid = ZendureSensor(self, "gridOffPower", None, "W", "power", "measurement")
         self.aggrOffGrid = ZendureRestoreSensor(self, "aggrGridOffPowerTotal", None, "kWh", "energy", "total", 2)
 
