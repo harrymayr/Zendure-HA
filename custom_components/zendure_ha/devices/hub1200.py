a4375f7bf52909c0ce27c2f882ef93041f2987a9
harrymayr <104222469+harrymayr@users.noreply.github.com>
2026-04-26 00:47:04 +0200

    Update Hub1200 limits and charging logic
    HUB Family can't do AC charging

--- custom_components/zendure_ha/devices/hub1200.py
+++ custom_components/zendure_ha/devices/hub1200.py
@@ -14,25 +14,20 @@ class Hub1200(ZendureLegacy):
     def __init__(self, hass: HomeAssistant, deviceId: str, prodName: str, definition: Any) -> None:
         """Initialise Hub1200."""
         super().__init__(hass, deviceId, prodName, definition["productModel"], definition)
-        """Can control the ACE 1500 with max 900W AC inputPower"""
-        self.setLimits(-900, 1200)
+        self.setLimits(0, 1200)
         self.maxSolar = -800
 
-    def batteryUpdate(self, batteries: list[ZendureBattery]) -> None:
-        # Check if any battery has kWh > 1
-        if any(battery.kWh > 1 for battery in batteries):
-            self.powerMin = -1200
-            self.limitInput.update_range(0, abs(self.powerMin))
-
     async def charge(self, power: int) -> int:
-        _LOGGER.info("Power charge %s => %s", self.name, power)
+        _LOGGER.info("AC Power charge %s not available => set power from %s to 0", self.name, power)
+        # The HUB family does not have AC charging possibility (even with ACE 1500), so set it to idle
+
         self.mqttInvoke(
             {
-                "arguments": [{"autoModelProgram": 2, "autoModelValue": power, "msgType": 1, "autoModel": 8}],
+                "arguments": [{"autoModelProgram": 0, "autoModelValue": 0, "msgType": 1, "autoModel": 0}],
                 "function": "deviceAutomation",
             }
         )
-        return power
+        return 0
 
     async def discharge(self, power: int) -> int:
         _LOGGER.info("Power discharge %s => %s", self.name, power)
